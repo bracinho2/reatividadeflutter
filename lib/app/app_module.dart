488345d6ca285f02396b74582cf371a201a25601
bracinho2 <bracinho2@hotmail.com>
2022-05-30 06:35:06 -0300

    Instalação do Flutter Modular

---
+++ lib/app/app_module.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class AppModule extends Module {
+  @override
+  final List<Module> imports = [];
+
+  @override
+  final List<ModularRoute> routes = [];
+}
---
+++ lib/app/core/core_module.dart
@@ -0,0 +1,11 @@
+import 'dart:html';
+
+import 'package:flutter_modular/flutter_modular.dart';
+
+class CoreModule extends Module {
+  @override
+  final List<Bind> binds = [
+    //firebase here;
+    //firebase service here;
+  ];
+}
